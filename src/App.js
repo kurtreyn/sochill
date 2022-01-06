b7c2c0cfb5afc60dab57ea7c6a00ad2568e21e04
kurtreyn <kurtreyn@yahoo.com>
2022-01-06 14:48:27 -0500

    started working on navigation, added favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/sass/style.css';
+import React, { useState } from 'react';
+import { HashRouter } from 'react-router-dom';
+
+import Main from './components/Main';
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <HashRouter>
+      <Main />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import navLogo from '../images/logo-sochill.png';
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar light expand="lg" className="custom-nav">
+        <NavbarBrand href="/">
+          <img src={navLogo} className="nav-logo" />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="/">Home</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/about">About</Link>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;

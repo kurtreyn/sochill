79fa64d163d416d66fbf509a789435f6b9e486be
kurtreyn <kurtreyn@yahoo.com>
2022-01-11 01:28:58 -0500

    style landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import logo from '../images/logo-sochill.png';
 import React from 'react';
 import Signup from './Signup';
+import Login from './Login';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
@@ -11,7 +13,14 @@ export default function LandingPage() {
           <img src={logo} alt="logo" />
         </div>
         <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 form-col">
-          <Signup />
+          {/* <Signup /> */}
+          <p>
+            <Link to="/signup">sign up</Link>
+          </p>
+
+          <p>
+            <Link to="/login">log in</Link>
+          </p>
         </div>
       </div>
     </div>

4f57098946b19b1acac0a5083ea5afbf9f5b24f8
KurtReyn <kurtreyn@yahoo.com>
2022-01-11 03:27:37 -0500

    started adding options to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,8 +3,46 @@ import React from 'react';
 import Signup from './Signup';
 import Login from './Login';
 import { Link } from 'react-router-dom';
+import loginIcon from '../images/icon-login.png';
+import signUpIcon from '../images/icon-signup.png';
 
-export default function LandingPage() {
+const InitialDisplay = () => {
+  return (
+    <div className="landing-icon-container">
+      <div>
+        <img
+          src={loginIcon}
+          alt="login"
+          data-option="login"
+          onClick={selectOption}
+        />
+        login
+      </div>
+      <div>
+        <img
+          src={signUpIcon}
+          alt="sign up"
+          data-option="signup"
+          onClick={selectOption}
+        />
+        sign up
+      </div>
+    </div>
+  );
+};
+
+function selectOption(e) {
+  let selection = '';
+  if (e.target.dataset === 'login') {
+    selection = <Login />;
+  } else if (e.target.dataset === 'signup') {
+    selection = <Signup />;
+  } else {
+    selection = <InitialDisplay />;
+  }
+}
+
+export default function LandingPage(selection) {
   return (
     <div className="container-fluid">
       <div className="row custom-row landing-row">
@@ -14,13 +52,7 @@ export default function LandingPage() {
         </div>
         <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 form-col">
           {/* <Signup /> */}
-          <p>
-            <Link to="/signup">sign up</Link>
-          </p>
-
-          <p>
-            <Link to="/login">log in</Link>
-          </p>
+          {selection}
         </div>
       </div>
     </div>
